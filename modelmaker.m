d6af46e3273129438e938d30271864240e6e1254
Matthew Iverson <miverson@vtti.vt.edu>
2018-11-10 13:44:37 -0700

    Added matlab predictor script and sample output

---
+++ modelmaker.m
@@ -0,0 +1,13 @@
+data = load('solarsample2016.csv');
+ghi = reshape(data(:,6), [24 365])';
+dailyData = sum(ghi, 2)/1000;
+
+mean(dailyData)
+modelFreq = fft(dailyData) .* [1;1;zeros(362,1);1];
+[modelFreq(end);modelFreq(1);modelFreq(2)]/365
+dailyModel = real(ifft(modelFreq));
+
+plot(1:365, dailyData, '*')
+hold on
+plot(1:365, dailyModel, 'r');
+save('dailyInsolation.csv', 'dailyModel')
